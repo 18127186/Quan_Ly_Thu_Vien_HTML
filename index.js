5a4ff1e01c8dfa53ebfc42fa38a3d6c7602591b2
18127186 <18127186@student.hcmus.edu.vn>
2020-12-22 00:08:47 +0700

    hbs for staff

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var models = require('./models');
 
+var tenaccount; // them cai nay vo header nha vidu chua dang nhap thi se la 'Dang nhap'
+var userLogin= {} //profile cua doi tuong dang login. null la admin
 //xu ly du lieu khi login
 var bodyParse = require ('body-parser')
 app.use(bodyParse.json())
@@ -55,13 +57,16 @@ app.get('/sync', function (req, res) {
 app.get('/Signin', function (req, res) {
     res.sendFile(__dirname+'/Signin.html')
 })
-
-app.get('/process',function(req,res){
-    console.log(req.query)
+app.get('/Staff',function(req,res){
+    res.locals.header = tenaccount
+    res.render('Staff',userLogin)
+})
+app.post('/Staff',function(req,res){
+    
     models.Account_staff.findOne({
         where:{
-            email: req.query.user,
-            cmnd: req.query.pass
+            email: req.body.user,
+            cmnd: req.body.pass
         }
     }).then(function(account){
         var data ={
@@ -78,45 +83,78 @@ app.get('/process',function(req,res){
             nguoilap: account.nguoilap,
             image: account.image
         }
+        userLogin = data;
         res.locals.header = 'Xin chào ' + data.hoten
+        tenaccount = res.locals.header
         res.render('Staff',data)
         
     }).catch(function(error){
-        if(req.query.user == '1@1' &&  req.query.pass == 'phuoc412'){
+        if(req.body.user == '1@1' &&  req.body.pass == 'phuoc412'){
             
             res.locals.header = 'Xin chào Boss '
+            tenaccount = 'Boss'
             res.render('admin')
         }
     })
 })
  //view profile of customer
- app.get('/Staff',function(req,res){
-    models.Account.findOne({
-        where:{
-            hoten: 'Trần văn A'
+ app.get('/Account',function(req,res){
+    models.Account.findAll().then(function(account){
+        var data=[]
+        for(i=0;i<account.length;i++){
+            var user ={
+                id: i+1,
+                hoten: account[i].hoten,
+                ngaysinh: account[i].ngaysinh,
+                cmnd: account[i].cmnd,
+                gioitinh: account[i].gioitinh,     
+                dantoc: account[i].dantoc,
+                ngaylap: account[i].ngaylap,      
+                sdt: account[i].sdt,
+                email: account[i].email,        
+                diachi: account[i].diachi,
+                sotiendatcoc: account[i].sotiendatcoc, 
+                nguoilap: account[i].nguoilap,
+                image: account[i].image
+            }
+            data.push(user)
         }
-    }).then(function(account){
-        var data ={
-            hoten: account.hoten,
-            ngaysinh: account.ngaysinh,
-            cmnd: account.cmnd,
-            gioitinh: account.gioitinh,     
-            dantoc: account.dantoc,
-            ngaylap: account.ngaylap,      
-            sdt: account.sdt,
-            email: account.email,        
-            diachi: account.diachi,
-            sotiendatcoc: account.sotiendatcoc, 
-            nguoilap: account.nguoilap,
-            image: account.image
-        }
-        console.log(data)
-        res.render('Staff',data)
+        res.locals.style = 'TableScroll.css'
+        res.locals.header = tenaccount
+        res.render('Account',{data:data})  
     }).catch(function(error){
-        console.log(error)
+        res.send(error)
     })
 
  })
+ app.get('/XoaSach',function(req,res){
+    models.Book.findAll().then(function(books){
+        var data=[]
+        for(i=0;i<books.length;i++){
+            var book ={
+                id: i+1,
+                tensach: books[i].tensach,
+                theloai: books[i].theloai,
+                tentacgia:  books[i].tentacgia
+            }
+            data.push(book)
+        }
+        res.locals.style = 'TableScroll.css'
+        res.locals.header = tenaccount
+        res.render('XoaSach',{data: data}) 
+    }).catch(function(error){
+        res.send(error)
+    })
+
+ })
+
+ 
+ app.get('/Sign_up_ThanhVien',function(req,res){
+    res.sendFile(__dirname+'/Sign_up_ThanhVien.html')
+})
+app.get('/NhapSach',function(req,res){
+    res.sendFile(__dirname+'/NhapSach.html')
+})
 //set port
 app.set('port', process.env.PORT | 5000)
 app.listen(app.get('port'), function () {
